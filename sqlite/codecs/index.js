f3d5522bda0b250a44ee0fe6bfa78ab6fb49c43d
Mwni <manuel@tigmar.de>
2022-06-23 00:00:18 +0200

    add enum codec
    this writes the value index to the database instead of the full value

--- sqlite/codecs/index.js
+++ sqlite/codecs/index.js
@@ -1,7 +1,9 @@
 import anyTypeCodec from './type-any.js'
 import booleanTypeCodec from './type-boolean.js'
+import enumCodec from './enum.js'
 
 export default [
 	anyTypeCodec,
-	booleanTypeCodec
+	booleanTypeCodec,
+	enumCodec
 ]
